715273b7c575283618eadca0384d060a64ccf714
Ryan Lee <drdgvhbh@gmail.com>
2019-05-08 19:53:00 -0400

    Add a stub android method channel for managing ETH wallets

--- android/app/src/main/kotlin/drdgvhbh/com/github/ethwallet/MainActivity.kt
+++ android/app/src/main/kotlin/drdgvhbh/com/github/ethwallet/MainActivity.kt
@@ -3,11 +3,21 @@ package drdgvhbh.com.github.ethwallet
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+
+private const val WALLET_CHANNEL = "drdgvhbh.com.ethwallet.wallet"
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    MethodChannel(flutterView, WALLET_CHANNEL).setMethodCallHandler{ call, result ->
+      when(call.method) {
+        "createWallet" -> result.success(Unit)
+        else -> result.notImplemented()
+      }
+    }
   }
 }
