d7104bb0aad1cfb70e6ad984f3c0ca476721aaa1
Ryan Lee <drdgvhbh@gmail.com>
2019-05-10 01:06:08 -0400

    Add missing "Named IO" annotation to IO Dispatcher

--- android/app/src/main/kotlin/drdgvhbh/com/github/ethwallet/di/module/DispatcherModule.kt
+++ android/app/src/main/kotlin/drdgvhbh/com/github/ethwallet/di/module/DispatcherModule.kt
@@ -4,11 +4,13 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 class DispatcherModule {
     @Provides
     @Singleton
+    @Named("IO")
     fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
